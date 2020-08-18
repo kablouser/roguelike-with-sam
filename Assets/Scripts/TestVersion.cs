cc830ee519f516c15028675da38c697488eb220b
kablouser <kablouser231@gmail.com>
2020-08-18 16:52:16 +0100

    2019 Rollback & Tilemap Setup

---
+++ Assets/Scripts/TestVersion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVersion : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Debug.Log("Hello");
+        }
+    }
+}
